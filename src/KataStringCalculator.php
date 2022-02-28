a64d61539e308fef9fa93fbbaa8e2d213deba04b
Mikel <mikelariceta13@gmail.com>
2022-02-28 08:04:36 +0000

    when_given_empty_string_return_0 TEST

---
+++ src/KataStringCalculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate;
+
+class KataStringCalculator
+{
+    public function add(String $number): String
+    {
+        return "0";
+    }
+}
---
+++ tests/KataStringCalculatorTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+use Deg540\PHPTestingBoilerplate\KataStringCalculator;
+use PHPUnit\Framework\TestCase;
+
+class KataStringCalculatorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function when_given_empty_string_return_0(){
+        $kataStringCalculator = new KataStringCalculator();
+
+        $result = add("");
+
+        $this->assertEquals("0", $result);
+    }
+}
